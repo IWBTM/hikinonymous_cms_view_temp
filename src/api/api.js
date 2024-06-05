429d25bcc3b55f8034c62c8f48844b3eece92095
GANG <rnlduadl2000@naver.com>
2024-06-05 19:38:05 +0900

    API 요청 헤더명 AccessToken -> Access-Token 수정

--- src/api/api.js
+++ src/api/api.js
@@ -4,9 +4,10 @@ const api = axios.create({
   baseURL: 'http://localhost:8082',
   headers: {
     "Content-Type": "application/json; charset=utf-8",
-    "AccessToken": localStorage.getItem("AccessToken")
+    "Access-Token": localStorage.getItem("AccessToken")
   },
 });
+
 api.defaults.withCredentials = true;
 
 export default api;
