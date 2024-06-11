98d9d0c4024a0554f465b6c53a5661f03a6d33e2
GANG <rnlduadl2000@naver.com>
2024-06-11 23:54:53 +0900

    CMS MENU LIST 조회 시점 변경

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Route,
   Switch,
@@ -25,18 +25,108 @@ import Charts from "../../pages/charts";
 
 // context
 import { useLayoutState } from "../../context/LayoutContext";
+import {
+  Accessibility as PrivacyIcon,
+  AccountTree as CmsMenuIcon,
+  BallotOutlined as BoardIcon,
+  CategoryOutlined as CategoryIcon,
+  CheckBox as TermIcon,
+  GridOn as CodeIcon,
+  Help as InquiryIcon, LiveHelp as FaqIcon, Notifications as NoticeIcon, NotInterested as DropMemberIcon,
+  OpenInBrowser as PopupIcon,
+  PersonOutline as MemberIcon,
+  SupervisorAccountOutlined as AdminIcon,
+  ViewAgenda as FrontMenuIcon,
+  VpnKey as AuthIcon,
+  Web as BannerIcon,
+  WebOutlined as SiteIcon,
+} from "@material-ui/icons";
+import api from "../../api/api";
 
 function Layout(props) {
   var classes = useStyles();
+  const [menuData, setMenuData] = useState([]);
 
   // global
   var layoutState = useLayoutState();
+
+  const setMenuIcon = (menuCode) => {
+    switch (menuCode) {
+      case 'ADMIN_MANAGEMENT': return <AdminIcon/>;
+      case 'SITE_MANAGEMENT': return <SiteIcon/>;
+      case 'MEMBER_MANAGEMENT': return <MemberIcon/>;
+      case 'CATEGORY_MANAGEMENT': return <CategoryIcon/>;
+      case 'BOARD_MANAGEMENT': return <BoardIcon/>;
+      case 'TERM_MANAGEMENT': return <TermIcon/>;
+      case 'CODE_MANAGEMENT': return <CodeIcon/>;
+      case 'CMS_MENU_MANAGEMENT': return <CmsMenuIcon/>;
+      case 'ADMIN_AUTH_MANAGEMENT': return <AuthIcon/>;
+      case 'FRONT_MENU_MANAGEMENT': return <FrontMenuIcon/>;
+      case 'POPUP_MANAGEMENT': return <PopupIcon/>;
+      case 'BANNER_MANAGEMENT': return <BannerIcon/>;
+      case 'INQUIRY_MANAGEMENT': return <InquiryIcon/>;
+      case 'FAQ_MANAGEMENT': return <FaqIcon/>;
+      case 'NOTICE_MANAGEMENT': return <NoticeIcon/>;
+      case 'DROP_MEMBER_MANAGEMENT': return <DropMemberIcon/>;
+      case 'PRIVACY_MANAGEMENT': return <PrivacyIcon/>;
+    }
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await api.get("/cms/menu/list");
+        if (response.data.code === 200) {
+          let data = response.data.data;
+          let menuList = [];
+          for (let i = 0; i < data.length; i++) {
+            let cmsMenu = {};
+            if (data[i].menuLevel === 1) {
+              cmsMenu = {
+                key: data[i].cmsMenuSeq,
+                id: data[i].cmsMenuSeq,
+                label: data[i].menuNm,
+                link: data[i].filePath,
+                icon: setMenuIcon(data[i].menuCode),
+                children: []
+              };
+              for (let j = 0; j < data.length; j++) {
+                if (data[j].authDir === data[i].authDir && data[j].menuLevel === 2) {
+                  cmsMenu.children.push({
+                    key: data[j].cmsMenuSeq,
+                    id: data[j].cmsMenuSeq,
+                    label: data[j].menuNm,
+                    link: data[j].filePath,
+                    icon: setMenuIcon(data[j].menuCode)
+                  });
+                }
+              }
+              menuList.push(cmsMenu);
+            }
+          }
+          setMenuData(menuList);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+  fetchData();
+  }, []);
+
+  const renderRoutes = () => {
+    return menuData.map((menu) =>
+      menu.children.map((subMenu) => (
+        <Route key={subMenu.id} path={subMenu.link} component={Tables}/>
+      ))
+    )
+  };
 
   return (
     <div className={classes.root}>
         <>
           <Header history={props.history} />
-          <Sidebar />
+          <Sidebar menuList={menuData} />
           <div
             className={classnames(classes.content, {
               [classes.contentShift]: layoutState.isSidebarOpened,
@@ -44,18 +134,7 @@ function Layout(props) {
           >
             <div className={classes.fakeToolbar} />
             <Switch>
-              <Route path="/app/dashboard" component={Dashboard} />
-              <Route path="/app/typography" component={Typography} />
-              <Route path="/app/tables" component={Tables} />
-              <Route path="/app/notifications" component={Notifications} />
-              <Route
-                exact
-                path="/app/ui"
-                render={() => <Redirect to="/app/ui/icons" />}
-              />
-              <Route path="/app/ui/maps" component={Maps} />
-              <Route path="/app/ui/icons" component={Icons} />
-              <Route path="/app/ui/charts" component={Charts} />
+              {renderRoutes()}
             </Switch>
           </div>
         </>
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -38,50 +38,14 @@ import {
   useLayoutDispatch,
   toggleSidebar,
 } from "../../context/LayoutContext";
-import api from "../../api/api";
 
-  // { id: 0, label: "Dashboard", link: "/app/dashboard", icon: <HomeIcon /> },
-  // {
-  //   id: 1,
-  //   label: "Typography",
-  //   link: "/app/typography",
-  //   icon: <TypographyIcon />,
-  // },
-  // { id: 2, label: "Tables", link: "/app/tables", icon: <TableIcon /> },
-  // {
-  //   id: 3,
-  //   label: "Notifications",
-  //   link: "/app/notifications",
-  //   icon: <NotificationsIcon />,
-  // },
-  // {
-  //   id: 4,
-  //   label: "UI Elements",
-  //   link: "/app/ui",
-  //   icon: <UIElementsIcon />,
-  //   children: [
-  //     { label: "Icons", link: "/app/ui/icons" },
-  //     { label: "Charts", link: "/app/ui/charts" },
-  //     { label: "Maps", link: "/app/ui/maps" },
-  //   ],
-  // },
-  // { id: 5, type: "divider" },
-  // { id: 6, type: "title", label: "HELP" },
-  // { id: 7, label: "Library", link: "", icon: <LibraryIcon /> },
-  // { id: 8, label: "Support", link: "", icon: <SupportIcon /> },
-  // { id: 9, label: "FAQ", link: "", icon: <FAQIcon /> },
-
-function Sidebar({ location }) {
+function Sidebar({ location, menuList }) {
   var classes = useStyles();
   var theme = useTheme();
 
   // global
   var { isSidebarOpened } = useLayoutState();
   var layoutDispatch = useLayoutDispatch();
-  var [structure, setStructure] = useState([]);
-  var [loading, setLoading] = useState(true);
-  var [error, setError] = useState(null);
-  var [errorMessage, setErrorMessage] = useState("");
 
   // local
   var [isPermanent, setPermanent] = useState(true);
@@ -89,91 +53,6 @@ function Sidebar({ location }) {
   useEffect(function() {
     window.addEventListener("resize", handleWindowWidthChange);
     handleWindowWidthChange();
-
-    const setMenuIcon = (menuCode) => {
-      let icon;
-      switch (menuCode) {
-        case 'ADMIN_MANAGEMENT': icon = <AdminIcon/>;
-          break;
-        case 'SITE_MANAGEMENT': icon = <SiteIcon/>;
-          break;
-        case 'MEMBER_MANAGEMENT': icon = <MemberIcon/>;
-          break;
-        case 'CATEGORY_MANAGEMENT': icon = <CategoryIcon/>;
-          break;
-        case 'BOARD_MANAGEMENT': icon = <BoardIcon/>;
-          break;
-        case 'TERM_MANAGEMENT': icon = <TermIcon/>;
-          break;
-        case 'CODE_MANAGEMENT': icon = <CodeIcon/>;
-          break;
-        case 'CMS_MENU_MANAGEMENT': icon = <CmsMenuIcon/>;
-          break;
-        case 'ADMIN_AUTH_MANAGEMENT': icon = <AuthIcon/>;
-          break;
-        case 'FRONT_MENU_MANAGEMENT': icon = <FrontMenuIcon/>;
-          break;
-        case 'POPUP_MANAGEMENT': icon = <PopupIcon/>;
-          break;
-        case 'BANNER_MANAGEMENT': icon = <BannerIcon/>;
-          break;
-        case 'INQUIRY_MANAGEMENT': icon = <InquiryIcon/>;
-          break;
-        case 'FAQ_MANAGEMENT': icon = <FaqIcon/>;
-          break;
-        case 'NOTICE_MANAGEMENT': icon = <NoticeIcon/>;
-          break;
-        case 'DROP_MEMBER_MANAGEMENT': icon = <DropMemberIcon/>;
-          break;
-        case 'PRIVACY_MANAGEMENT': icon = <PrivacyIcon/>;
-          break;
-      }
-      return icon;
-    }
-
-    const fetchData = async () => {
-      try {
-        const response = await api.get("/cms/menu/list");
-        if (response.data.code === 200) {
-          let data = response.data.data;
-          let menuList = [];
-          for (let i = 0; i < data.length; i++) {
-            let cmsMenu = {};
-            if (data[i].menuLevel === 1) {
-              cmsMenu = {
-                key: data[i].cmsMenuSeq,
-                id: data[i].cmsMenuSeq,
-                label: data[i].menuNm,
-                link: data[i].filePath,
-                icon: setMenuIcon(data[i].menuCode),
-                children: []
-              };
-              for (let j = 0; j < data.length; j++) {
-                if (data[j].authDir === data[i].authDir && data[j].menuLevel === 2) {
-                  cmsMenu.children.push({
-                    key: data[j].cmsMenuSeq,
-                    id: data[j].cmsMenuSeq,
-                    label: data[j].menuNm,
-                    link: data[j].filePath,
-                    icon: setMenuIcon(data[j].menuCode)
-                  });
-                }
-              }
-              menuList.push(cmsMenu);
-            }
-            console.log('cmsMenu:: ', cmsMenu);
-          }
-          setStructure(menuList);
-          setLoading(false);
-        }
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        setError(true);
-        setLoading(false);
-      }
-    };
-
-    fetchData();
 
     return function cleanup() {
       window.removeEventListener("resize", handleWindowWidthChange);
@@ -206,7 +85,7 @@ function Sidebar({ location }) {
         </IconButton>
       </div>
       <List className={classes.sidebarList}>
-        {structure.map(link => (
+        {menuList.map(link => (
           <SidebarLink
             key={link.id}
             location={location}
@@ -229,14 +108,6 @@ function Sidebar({ location }) {
     } else if (!isSmallScreen && !isPermanent) {
       setPermanent(true);
     }
-  }
-
-  if (loading) {
-    return <Typography>Loading...</Typography>;
-  }
-
-  if (error) {
-    return <Typography color="error">An error occurred while fetching data.</Typography>;
   }
 
 }
